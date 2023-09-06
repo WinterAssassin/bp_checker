fff9b50fddb84a8b0a51ce43f91cd8b83d779f2e
MJ Cabrera <mjcabrera1996@gmail.com>
2023-09-06 09:56:33 +0800

    Refactor code, add reading cateagory

--- bp_checker/lib/models/bp_category.dart
+++ bp_checker/lib/models/bp_category.dart
@@ -9,41 +9,40 @@ class BPCategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return switch (reading) {
-      PressureCategory.none => Container(
-          width: 200,
-          height: 50,
-          color: const Color.fromRGBO(204, 204, 204, 1),
-        ),
-      PressureCategory.low => Container(
-          width: 200,
-          height: 50,
-          color: const Color.fromRGBO(173, 216, 230, 1),
-        ),
-      PressureCategory.normal => Container(
-          width: 200,
-          height: 50,
-          color: const Color.fromRGBO(166, 206, 57, 1),
-        ),
-      PressureCategory.elevated => Container(
-          width: 200,
-          height: 50,
-          color: const Color.fromRGBO(255, 236, 0, 1),
-        ),
-      PressureCategory.h1 => Container(
-          width: 200,
-          height: 50,
-          color: const Color.fromRGBO(255, 182, 0, 1),
-        ),
-      PressureCategory.h2 => Container(
-          width: 200,
-          height: 50,
-          color: const Color.fromRGBO(186, 58, 2, 1),
-        ),
-      PressureCategory.hCrisis => Container(
-          width: 200,
-          height: 50,
-          color: const Color.fromRGBO(153, 7, 17, 1),
-        ),
+      PressureCategory.none => const ReadingCategory(
+          text: 'None', catColor: Color.fromRGBO(204, 204, 204, 1)),
+      PressureCategory.low => const ReadingCategory(
+          text: 'Low', catColor: Color.fromRGBO(173, 216, 230, 1)),
+      PressureCategory.normal => const ReadingCategory(
+          text: 'Normal', catColor: Color.fromRGBO(166, 206, 57, 1)),
+      PressureCategory.elevated => const ReadingCategory(
+          text: 'Elevated', catColor: Color.fromRGBO(255, 236, 0, 1)),
+      PressureCategory.h1 => const ReadingCategory(
+          text: 'Hypertension Stage 1',
+          catColor: Color.fromRGBO(255, 182, 0, 1)),
+      PressureCategory.h2 => const ReadingCategory(
+          text: 'Hypertension Stage 2',
+          catColor: Color.fromRGBO(186, 58, 2, 1)),
+      PressureCategory.hCrisis => const ReadingCategory(
+          text: 'Hypertensive Crisis', catColor: Color.fromRGBO(153, 7, 17, 1)),
     };
+  }
+}
+
+class ReadingCategory extends StatelessWidget {
+  final String text;
+  final Color catColor;
+
+  const ReadingCategory(
+      {super.key, required this.text, required this.catColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 50,
+      color: catColor,
+      child: Center(child: Text(text)),
+    );
   }
 }
